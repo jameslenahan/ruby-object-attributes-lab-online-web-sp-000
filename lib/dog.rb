fc451226d420c8c6514d2df0cebbc83456e63ed6
James Lenahan <james.k.lenahan@gmail.com>
2019-12-04 18:09:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,3 +14,18 @@ class Dog
     @breed
   end
 end
+
+class Person
+  def name=(persons_name)
+    @name = persons_name
+  end
+  def name
+    @name
+  end
+  def job=(persons_job)
+    @job = persons_job
+  end
+  def job
+    @job
+  end
+end
